69e234bce1b5d76fe5d8cbf56b20a930a1a46a50
loully <holy.raza@gmail.com>
2025-11-04 12:30:43 +0100

    [feat] Create User Repository with jpa annotation

---
+++ src/main/java/com/lab4tech/hive/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.lab4tech.hive.repository;
+
+import com.lab4tech.hive.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
