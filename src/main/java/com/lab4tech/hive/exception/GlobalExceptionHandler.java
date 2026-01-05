e0f7b5ff7efc7c2e1475e4a24701db6d61a64bc2
loully <holy.raza@gmail.com>
2026-01-05 10:38:06 +0100

    [feat] Add all Exception handlers

--- src/main/java/com/lab4tech/hive/exception/GlobalExceptionHandler.java
+++ src/main/java/com/lab4tech/hive/exception/GlobalExceptionHandler.java
@@ -12,4 +12,69 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleOverlappingAvailability(OverlappingAvailabilityException exception){
         return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
     }
+
+    @ExceptionHandler(VolunteerAvailabilityAlreadyExistsException.class)
+    public ResponseEntity<String> handleVolunteerAvailabilityAlreadyExists(VolunteerAvailabilityAlreadyExistsException exception){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(VolunteerProfileNotFoundException.class)
+    public ResponseEntity<String> handleVolunteerProfileNotFound(VolunteerProfileNotFoundException exception){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(VolunteerProfileAlreadyExistsException.class)
+    public ResponseEntity<String> handleVolunteerProfileAlreadyExists(VolunteerProfileAlreadyExistsException exception){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(MissionAlreadyExistsException.class)
+    public ResponseEntity<String> handleMissionAlreadyExists(MissionAlreadyExistsException exception){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(MissionNotFoundException.class)
+    public ResponseEntity<String> handleMissionNotFound(MissionNotFoundException exception){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(MissionAlreadyAssignedException.class)
+    public ResponseEntity<String> handleMissionAlreadyAssigned(MissionAlreadyAssignedException exception){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(MissionFullException.class)
+    public ResponseEntity<String> handleMissionFull(MissionFullException exception){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(MissionExpiredException.class)
+    public ResponseEntity<String> handleMissionExpired(MissionExpiredException exception){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(VolunteerNotAvailableException.class)
+    public ResponseEntity<String> handleVolunteerNotAvailable(VolunteerNotAvailableException exception){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(SkillNotFoundException.class)
+    public ResponseEntity<String> handleSkillNotFound(SkillNotFoundException exception){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(SkillAlreadyExistsException.class)
+    public ResponseEntity<String> handleSkillAlreadyExists(SkillAlreadyExistsException exception){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFound(UserNotFoundException exception){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<String> handleUserAlreadyExists(UserAlreadyExistsException exception){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
+    }
 }
