a6c39f16db556c1b88c3b69773a64dce27e67e10
loully <holy.raza@gmail.com>
2026-01-05 10:36:25 +0100

    [refacto] delete unused imports

--- src/main/java/com/lab4tech/hive/HiveApplication.java
+++ src/main/java/com/lab4tech/hive/HiveApplication.java
@@ -4,15 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
 @EntityScan("com.lab4tech.hive.model.entity")
 @SpringBootApplication
 public class HiveApplication {
